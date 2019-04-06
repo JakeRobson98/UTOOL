dae6d1599575a2f8baf48cbc4eda81f0dcf2a932
JakeRobson98 <jjoerobson@gmail.com>
2019-04-06 13:48:10 +1300

    taylors a gaylord

--- src/api/index.js
+++ src/api/index.js
@@ -3,17 +3,22 @@ import { API_ENDPOINT } from './config';
 
 
 
-export const registerUser = (email, pass) => {
-  console.log(email + pass)
-  return axios.post(`${API_ENDPOINT}/register`, {
-    email: email,
-    password: pass 
-  })
-}
-
 // export const registerUser = (email, pass) => {
-//   return axios.post(`${API_ENDPOINT}/register/email=${email}&&password=${pass}`);
+//   console.log(email + pass)
+//   return axios.post(`${API_ENDPOINT}/register`, {
+//     email: email,
+//     password: pass 
+//   }).then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
 // }
+
+export const registerUser = (email, pass) => {
+  return axios.post(`${API_ENDPOINT}/register/email=${email}&password=${pass}`);
+}
 export const loginUser = (user) => {
   return axios.post(`${API_ENDPOINT}/login/${user}/`);
  }
